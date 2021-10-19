b4254b84c4bb6020e3814f34e241e720d68553d2
Raitis Barkans <raitisbarkans@gmail.com>
2021-10-19 13:59:52 +0300

    Add sample task 6

--- src/selenium/sample/Sample6Task.java
+++ src/selenium/sample/Sample6Task.java
@@ -34,21 +34,46 @@ public class Sample6Task {
     public void findElementByXPath() throws Exception {
 //         TODO:
 //        2 ways to find text: "Heading 2 text":
+        driver.findElement(By.xpath("//h2[@id=\"heading_2\"]"));
+        driver.findElement(By.xpath("//*[text()=\"Heading 2 text\"]"));
 //        1-2 ways to find text: "Test Text 1"
+        driver.findElement(By.xpath("//div[contains(@class,test1)]/p[contains(text(),\"Test Text 1\")]"));
+        driver.findElement(By.xpath("//*[@id=\"test1\"]/p[@class=\"test\"]"));
 //        1-2 ways to find text: "Test Text 2"
+        driver.findElement(By.xpath("//p[@class=\"twoTest\"]"));
+        driver.findElement(By.xpath("//p[text()=\"Test Text 2\"]"));
 //        1-2 ways to find text: "Test Text 3"
+        driver.findElement(By.xpath("//p[text()=\"Test Text 3\"]"));
+        driver.findElement(By.xpath("//div[@id=\"test3\"]/p"));
 //        1-2 ways to find text: "Test Text 4"
+        driver.findElement(By.xpath("//div[@id=\"test3\"]/p[2]"));
+        driver.findElement(By.xpath("//p[text()=\"Test Text 4\"]"));
 //        1-2 ways to find text: "Test Text 5"
+        driver.findElement(By.xpath("//p[text()=\"Test Text 5\"]"));
+        driver.findElement(By.xpath("//div[@id=\"test2\"]/p"));
 //        1-2 ways to find text: "This is also a button"
+        driver.findElement(By.xpath("//input[2]"));
+        driver.findElement(By.xpath("//input[contains(@value,\"also\")]"));
+
     }
 
     @Test
     public void findElementByCssName() throws Exception {
 //         TODO:
 //        1-2 ways to find text: "Heading 2 text"
+        driver.findElement(By.cssSelector("#heading_2"));
+        driver.findElement(By.cssSelector("h2:last-of-type"));
 //        1-2 ways to find text: "Test Text 1"
+        driver.findElement(By.cssSelector("#test1 p.test"));
+        driver.findElement(By.cssSelector("#test1 > p.test"));
 //        1-2 ways to find text: "Test Text 2"
+        driver.findElement(By.cssSelector("#test1 > p:last-child"));
+        driver.findElement(By.cssSelector("p.twoTest"));
 //        1-2 ways to find text: "Test Text 3"
+        driver.findElement(By.cssSelector("#test3 > p:nth-child(1)"));
+        driver.findElement(By.cssSelector("div#test3 > .test"));
 //        1-2 ways to find text: "This is also a button"
+        driver.findElement(By.cssSelector("input[id=buttonId]"));
+        driver.findElement(By.cssSelector("input[value=\"This is also a button\"]"));
     }
 }
